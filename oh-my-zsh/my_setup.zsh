d92ff487185fce8d2b8345fd549a31c12bf95619
rapha4.lx <rapha4.lx@gmail.com>
2026-04-29 21:04:22 -0300

    Remove lazydocker lz alias

--- oh-my-zsh/my_setup.zsh
+++ oh-my-zsh/my_setup.zsh
@@ -13,7 +13,6 @@ alias vi="nvim"
 alias v="nvim"
 alias ll="ls -lah"
 alias lg="lazydocker"
-alias lz="lazydocker"
 
 ctls() {
   docker ps -a --format 'table {{.Names}}\t{{.Status}}\t{{.CreatedAt}}'
